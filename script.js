4b9850253d52bb7cd49ea7253679e8156e222eb9
winkels7 <hmaster.studio@gmail.com>
2022-10-25 18:09:46 -0300

    Inicialiazação do IndexedDB

---
+++ script.js
@@ -0,0 +1,57 @@
+const indexedDB =
+    window.indexedDB ||
+    window.mozIndexedDB ||
+    window.webkitIndexedDB ||
+    window.msIndexedDB ||
+    window.shimIndexedDB;
+
+const request = indexedDB.open("MerceariaDB", 1);
+
+request.onerror = function (event){
+    console.error("Um erro ocorreu com o IndexedDB");
+    console.error(event);
+}
+
+request.onupgradeneeded = function() {
+    const db = request.result;
+    const store = db.createObjectStore("produto", {keyPath: "id"});
+    store.createIndex("nome_produto", ["nome"], {unique: false});
+    store.createIndex("valor_produto", ["valor"], {unique: false});
+}
+
+request.onsuccess = function (){
+    const db = request.result;
+    const transacao = db.transaction("produto", "readwrite");
+
+    const store = transacao.objectStore("produto");
+    const indexNomeProd = store.index("nome_produto");
+    const indexValorProd = store.index("valor_produto");
+
+
+    //Inserindo dados na db
+
+    store.put({id: 1, nome: "Bolo", valor: "20"});
+    store.put({id: 2, nome: "Bolo", valor: "15"});
+    store.put({id: 3, nome: "Cupcake", valor: "4.5"});
+
+    //Fazendo query dos valores
+
+    const idQuery = store.get(1);
+
+    const puxarNome = indexNomeProd.getAll(["Bolo"]);
+    const puxarValor = indexValorProd.get(["20"]);
+
+    idQuery.onsuccess = function(){
+        console.log('idQuery', idQuery.result);
+    };
+    idQuery.onsuccess = function(){
+        console.log('puxarNome', puxarNome.result);
+    };
+    idQuery.onsuccess = function(){
+        console.log('puxarValor', puxarValor.result);
+    };
+
+    transacao.oncomplete = function(){
+        db.close();
+    };
+}
