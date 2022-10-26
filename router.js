b5992fc77c76aef13af0f85fa526cb048a0eca07
Joao Winkelmann <hmaster.studio@gmail.com>
2022-10-26 11:45:45 -0300

    Estrutura do Router do Vue

--- router.js
+++ router.js
@@ -1,31 +1,23 @@
-// 1. Define route components.
-// These can be imported from other files
-const Home = { template: '<div>Home</div>' }
-const Despesas = { template: '<div>About</div>' }
 
-// 2. Define some routes
-// Each route should map to a component.
-// We'll talk about nested routes later.
-const routes = [
-  { path: '/', component: Home },
-  { path: '/despesas', component: Despesas },
-]
-
-// 3. Create the router instance and pass the `routes` option
-// You can pass in additional options here, but let's
-// keep it simple for now.
-const router = VueRouter.createRouter({
-  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
-  history: VueRouter.createWebHashHistory(),
-  routes, // short for `routes: routes`
-})
-
-// 5. Create and mount the root instance.
-const app = Vue.createApp({})
-// Make sure to _use_ the router instance to make the
-// whole app router-aware.
-app.use(router)
-
-app.mount('#app')
-
-// Now the app has started!
+          const Dashboard = { template: 'Dashboard' }
+          const Planejamento = { template: 'Planejamento'}
+          const Produtos = { template: 'Produtos'}
+          const Despesas = { template: 'Aqui serão mostradas as despesas' }
+          
+          const routes = [
+            { path: '/', component: Dashboard },
+            { path: '/planejamento', component: Planejamento },
+            { path: '/produtos', component: Produtos },
+            { path: '/despesas', component: Despesas },
+          ]
+          
+          const router = VueRouter.createRouter({
+            history: VueRouter.createWebHashHistory(),
+            routes,
+          })
+          
+          const app = Vue.createApp({})
+          app.use(router)
+          
+          app.mount('#app')
+          
